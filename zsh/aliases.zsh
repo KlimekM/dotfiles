4b67e43bd9f3ebe29086b585616ab5c0260998f1
Michal Klimek <michal.klimek@zendesk.com>
2024-05-31 12:55:29 -0500

    Create base zsh dotfiles directory

---
+++ zsh/aliases.zsh
@@ -0,0 +1,8 @@
+# Reload ZSH after making changes
+alias szsh="source ~/.zshrc"
+
+# Open .zshrc
+alias ozshrc="code ~/.zshrc"
+
+# Open .oh-my-zsh/aliases.zsh
+alias oaliases="code ~/.oh-my-zsh/custom/aliases.zsh"
